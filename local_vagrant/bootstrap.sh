f734bcad8b041f89f4be4d886cc76805a974770b
vagrant <sidhemail@gmail.com>
2016-06-05 07:53:14 +0000

    fixed configuration files for Vagrant.

--- local_vagrant/bootstrap.sh
+++ local_vagrant/bootstrap.sh
@@ -3,7 +3,7 @@
 yum install epel-release -y
 yum install net-tools ansible git awscli -y
 
-cd /srv/server-config/local_vagrant/
-ansible-playbook -v -i hosts/localdev localdev.yml
+cd /srv/server-config
+ansible-playbook -v -i hosts/localdev sys_localdev.yml
 #ansible-playbook -v -i hosts/localdev deploy_localdev.yml --extra-vars "app_deploy_target=develop"
 
