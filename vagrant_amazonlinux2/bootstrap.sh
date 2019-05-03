67c033e1c429db9b3f908f0b515d744beff8050f
datemaki <sidhemail@gmail.com>
2019-05-03 14:08:30 +0900

    modified roles for Amazon Linux 2.

---
+++ vagrant_amazonlinux2/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#
+# This bootstrap script will be run on an Amazon Linux 2 instance.
+#
+
+amazon-linux-extras install epel
+yum install git unzip -y
+yum install ansible -y
+
+cd ./local_workspace/ansible/
+ansible-playbook -v -i hosts/local sys_local.yml
