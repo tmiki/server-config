fe531d246d6f032752b322acf06846c93010f6ce
tmiki <sidhemail@gmail.com>
2020-07-05 18:35:18 +0900

    added a new Vagrant file for the Ubuntu 20.04 LTS.

---
+++ vagrant_ubuntu20/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#
+# This bootstrap script will be run on an Ubuntu 20.04 LTS instance.
+#
+
+#amazon-linux-extras install epel
+apt install git unzip -y
+apt install ansible -y
+
+cd ./local_workspace/ansible/
+ansible-playbook -v -i hosts/local sys_local_minimum.yml
